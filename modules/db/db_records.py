27e04ac4f5cb6fece5cf8818fd1892d5ac91eab9
Javi Piay <javier4piay@gmail.com>
2026-05-18 17:30:16 +0200

    conflict1

--- modules/db/db_records.py
+++ modules/db/db_records.py
@@ -13,7 +13,21 @@ def get_records_db(as_df: bool = True):
     """
 
     zonas_anatomicas_df = load_catalog_list_db("zonas_anatomicas", as_df=True)
-    map_zonas = dict(zip(zonas_anatomicas_df["id"], zonas_anatomicas_df["nombre"]))
+
+    if (
+        zonas_anatomicas_df is None
+        or zonas_anatomicas_df.empty
+        or "id" not in zonas_anatomicas_df.columns
+        or "nombre" not in zonas_anatomicas_df.columns
+    ):
+        map_zonas = {}
+    else:
+        map_zonas = dict(
+            zip(
+                zonas_anatomicas_df["id"],
+                zonas_anatomicas_df["nombre"]
+            )
+        )
 
     # ----------------------------
     # Control de acceso reutilizable
